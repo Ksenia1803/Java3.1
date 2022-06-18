0595c385a2ab1de6f46f3181eb6c768d887a4af2
Ксения Тютчева <dmitrievna13@mail.ru>
2022-06-18 11:53:24 +0700

    src

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 65852;
+        int mile = price / 20;
+        System.out.println("Бонусные мили =" + mile);
+    }
+
+}
